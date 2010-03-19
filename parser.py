619fbd8012844c4f324d844d4746f8a18b32401b
Mike Griffith <bikegriffith@gmail.com>
2010-03-19 13:26:50 -0400

    refactor

--- parser.py
+++ parser.py
@@ -1,8 +1,17 @@
 
+
 def parse(input, schema):
-    if "-x" in input:
-        return {"x": True}
+    args = input.split(" ")
+    return _parse_list_of_args(args, schema)
+
+
+def _parse_list_of_args(args, schema):
     results = {}
+    for key, transform in schema.items():
+        expected_arg = "-%s" % key
+        if expected_arg in args:
+            results[key] = True
     return results
+
 
 
